a0b0894b4dbf094ecefc99bc88d14fd76a944815
francislibra <francislibra@gmail.com>
2015-05-12 08:32:46 -0300

    atualizador config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 */
 
-$config['base_url']	= 'http://localhost/sabitu';
+$config['base_url']	= 'http://ict.unifesp.br/sabitu';
 $config['app_name']	= 'SABITU';
 $config['app_version']	= 'beta 1.0.0';
 $config['app_desc']	= 'Gerador de Formulários';
