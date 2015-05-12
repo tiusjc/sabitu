2fe0efb91c4b487ad2cc392eec6f74ffe876d77f
francislibra <francislibra@gmail.com>
2015-05-12 11:39:09 -0300

    fix usuarios_id para usuario_id

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -39,11 +39,11 @@
                 $this->load->view( 'login_view', $data );
             }else{
           
-                $usuarios_id = $this->usuarios_model->validarUsuario_id();
+                $usuario_id = $this->usuarios_model->validarUsuario_id();
         
-                if ( $usuarios_id ){
+                if ( $usuario_id ){
                     
-                    $adm   = $this->adm_model->validate_adm( $usuarios_id );
+                    $adm   = $this->adm_model->validate_adm( $usuario_id );
                     
                     $form_id      = $this->input->post('form_id');
                     $form_nome    = $this->input->post('form_nome');
@@ -70,11 +70,11 @@
 
                         if( $form_id ){ 
                         
-                            $form_cadastro_id = $this->form_cadastro_model->validarForm_cadastro_id($usuarios_id, $form_id, $form_sigla);
+                            $form_cadastro_id = $this->form_cadastro_model->validarForm_cadastro_id($usuario_id, $form_id, $form_sigla);
                             
                             $this->session->set_userdata( 'form_cadastro_id', $form_cadastro_id );
                             
-                            if( $this->usuarios_model->validarUsuario_nome( $usuarios_id ) ){
+                            if( $this->usuarios_model->validarUsuario_nome( $usuario_id ) ){
                                   
                                 if( $form_cadastro_id ){ 
                                     
