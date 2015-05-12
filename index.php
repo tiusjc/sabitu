46115404ea0b0f7539aa4cf2a83d8f07e34a1c0f
francislibra <francislibra@gmail.com>
2015-05-12 09:09:49 -0300

    Colocado System dentro do projeto

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = '../base_ci/system';
+	$system_path = 'system';
 
 /*
  *---------------------------------------------------------------
