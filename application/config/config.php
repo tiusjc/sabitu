db415756a67c627fbc1259b8e7ffba3b4383c198
francislibra <francislibra@gmail.com>
2015-05-13 14:13:00 -0300

    alteração config cobaia

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ $config['base_url']	= 'http://localhost/sabitu';
 >>>>>>> 89e54040040d19e32c50c83256c4ca8ab0e6c0d5
 $config['base_url']	= 'http://ict.unifesp.br/sabitu';
 >>>>>>> a95a825cf8ba5aba53188ec5d72fd9e769dc4e79
-$config['app_name']	= 'SABITU';
+$config['app_name']	= 'SABITU Cobaia';
 $config['app_version']	= 'beta 1.0.0';
 $config['app_desc']	= 'Gerador de Formulários';
 
