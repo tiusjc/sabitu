88c7ff81ce6330910c233ced57f8ab67d0725e74
francislibra <francislibra@gmail.com>
2015-05-14 12:36:03 -0300

    ssss

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,65 +2,60 @@
 <?php
 class MY_Controller extends CI_Controller {
 	protected $crud;
-		
+
 function __construct($check_login = TRUE){
        parent::__construct();
-			
+
       if( $check_login )
 	$this->check_login();
-      		    
+
         $this->crud  = new grocery_CRUD();
-=======
-<<<<<<< HEAD
+
 <?php
 	class MY_Controller extends CI_Controller {
 		protected $crud;
-		
+
 		function __construct($check_login = TRUE){
 			parent::__construct();
-			
+
       if( $check_login )
 				$this->check_login();
-      		    
+
       $this->crud  = new grocery_CRUD();
->>>>>>> db415756a67c627fbc1259b8e7ffba3b4383c198
     	$this->crud->set_language('pt-br.portuguese');
     	$this->crud->set_theme('flexigrid');
     	$this->output->set_template('gerenciar');
 
       if ( !$this->adm ){
-      
+
         if( $this->crud->getState() == 'edit' ){
-          
+
           $id_check = $this->uri->segment(4);
 
           $this->check_id( $id_check );
         }
       }
 
-<<<<<<< HEAD
 }
-=======
+
 		}
->>>>>>> db415756a67c627fbc1259b8e7ffba3b4383c198
 
 
     private function check_id( $id_check ){
-      
+
       if( $id_check != $this->check_id ){
         redirect(site_url($this->uri->segment(1) ) );
       }
 
     }
- 
+
 		private function check_login(){
 			$logado = $this->session->userdata('logado');
 			if (!isset($logado) || $logado != true) {
             	redirect("erroacesso");
         	}
       else{
-<<<<<<< HEAD
-          $this->check_id         = $this->session->userdata('check_id'    );  
+          $this->check_id         = $this->session->userdata('check_id'    );
       	  $this->usuarios_id      = $this->session->userdata('usuarios_id' );
           $this->email            = $this->session->userdata('email'       );
           $this->adm              = $this->session->userdata('adm'         );
@@ -70,14 +65,13 @@ function __construct($check_login = TRUE){
           $this->form_existe      = $this->session->userdata('form_existe' );
           $this->form_rodape      = $this->session->userdata('form_rodape' );
 	  $this->form_cadastro_id = $this->session->userdata('form_cadastro_id'            );
-=======
-          $this->check_id         = $this->session->userdata('check_id'                );  
+          $this->check_id         = $this->session->userdata('check_id'                );
 
       		$this->usuario_id       = $this->session->userdata('usuario_id'            );
           $this->email            = $this->session->userdata('email'                   );
 
           $this->adm              = $this->session->userdata('adm'        );
-          
+
           $this->form_id          = $this->session->userdata('form_id'    );
           $this->form_nome        = $this->session->userdata('form_nome'  );
           $this->form_sigla       = $this->session->userdata('form_sigla' );
@@ -85,12 +79,11 @@ function __construct($check_login = TRUE){
           $this->form_rodape      = $this->session->userdata('form_rodape');
 
       		$this->form_cadastro_id = $this->session->userdata('form_cadastro_id'            );
->>>>>>> db415756a67c627fbc1259b8e7ffba3b4383c198
       }
 		}
 
 
-    	
+
 	}
 <<<<<<< HEAD
 ?>
