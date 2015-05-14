cc387d58a6e3009db8ed4ae9703b723467655bc6
francislibra <francislibra@gmail.com>
2015-05-14 10:05:25 -0300

    atualizar config-dev

--- hooks/config-dev.php
+++ hooks/config-dev.php
@@ -32,4 +32,4 @@
 	$arrConfig['SABITU-DEV'] = array(
 		'repository' => 'sabitu',
         'branch'     => 'dev',
-    	'execute'    => array('cd /var/www/sabitu-dev; git reset --hard 2>&1; git clean -f -d 2>&1;git pull producao dev 2>&1'));
+    	'execute'    => array('ssh -t tiusjc@lab.unifesp.br -p 2244 "/var/www/exec_git_sabitu"'));
