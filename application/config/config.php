c872a55aa14f4f1926beeb0e796b730b3ade472d
francislibra <francislibra@gmail.com>
2015-05-20 13:59:06 -0300

    sss

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@
 */
 
 $config['base_url']	= 'http://ict.unifesp.br/sabitu';
-$config['app_name']	= 'SABITU';
+$config['app_name']	= 'SABITUx';
 $config['app_vers']	= 'Beta 1.0.0';
 $config['app_desc']	= 'Gerador de Formulários';
 
