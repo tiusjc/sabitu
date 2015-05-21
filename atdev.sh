5c8a9c16dd77af580134866699602df351ccbc6b
francislibra <francislibra@gmail.com>
2015-05-21 13:11:36 -0300

    atualizar master

---
+++ atdev.sh
@@ -0,0 +1,5 @@
+echo "atualizar dev">>dev
+git add .
+git commit -m "atualizar dev"
+git push origin dev
+
---
+++ atmaster.sh
@@ -0,0 +1,5 @@
+echo "atualizar master">>master
+git add .
+git commit -m "atualizar master"
+git push origin master
+
